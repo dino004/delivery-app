41674d1e37737cedc88eb003d907ce22c6bd97bf
MedniyON <dmytro4lytvyn@gmail.com>
2026-03-26 21:26:52 +0200

    feat: implement product listing, shop filtering, and persistent cart logic

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,5 +1,7 @@
+
 const listProducts = document.querySelector(".list-menu");
 const shopList = document.querySelector(".list-shops");
+const addToCartBtn = document.querySelector(".add-btn");
 
 async function getProducts(shopName = "") {
   listProducts.innerHTML = "<p>Loading products...</p>";
@@ -19,8 +21,8 @@ async function getProducts(shopName = "") {
     listProducts.innerHTML = "";
 
     if (data.length === 0) {
-        listProducts.innerHTML = "<p>No products found in this shop.</p>";
-        return;
+      listProducts.innerHTML = "<p>No products found in this shop.</p>";
+      return;
     }
     data.forEach(({ name, price, image, shop }) => {
       const cardProduct = markupProductCard(name, price, image, shop);
@@ -35,6 +37,7 @@ async function getProducts(shopName = "") {
 shopList.addEventListener("click", isSelectedShop);
 
 function isSelectedShop(evt) {
+    evt.preventDefault();
   const button = evt.target.closest(".shop-btn");
 
   if (!button || !evt.currentTarget.contains(button)) return;
@@ -46,19 +49,62 @@ function isSelectedShop(evt) {
 getProducts();
 
 function markupProductCard(name, price, image, shop) {
-  return ` <li class="card">
+    const cart = JSON.parse(localStorage.getItem("order")) || [];
+    const isSelected = cart.some(item => item.name === name);
+
+    const btnText = isSelected ? "In Cart" : "add to Cart";
+    const btnDisabled = isSelected ? "disabled" : "";
+    const btnClass = isSelected ? "add-btn is-selected" : "add-btn";
+
+  return ` <li class="card" data-shop="${shop}">
               <img
                 src="${image}"
                 alt="${name}"
                 width="200"
                 class="image-card"
               />
-              <h3 class="card-title">${name} (from ${shop})</h3>
+              <h3 class="card-title">${name}</h3>
+              <p class="cart-shop">${shop}</p>
               <p>Price: ${price}</p>
-              <button class="add-btn">add to Cart</button>
+              <button class="${btnClass}" ${btnDisabled}>${btnText}</button>
             </li>`;
 }
 
 function render(product) {
   listProducts.insertAdjacentHTML("beforeend", product);
+}
+
+listProducts.addEventListener("click", (evt) => {
+  const button = evt.target.closest(".add-btn");
+  if (!button) return;
+
+  const card = button.closest(".card");
+  
+  const product = {
+    name: card.querySelector(".card-title").textContent,
+    price: card.querySelector("p").textContent.split(": ")[1],
+    image: card.querySelector(".image-card").src,
+    shop: card.dataset.shop,
+  };
+
+  addToCart(product, button);
+});
+
+function addToCart(product, button) {
+  let cart = JSON.parse(localStorage.getItem("order")) || [];
+
+  if (cart.length > 0 && cart[0].shop !== product.shop) {
+    alert("Please select products from the same shop or clear your cart!");
+    return;
+  }
+
+  const isExist = cart.some(item => item.name === product.name);
+  if (!isExist) {
+    cart.push(product);
+    localStorage.setItem("order", JSON.stringify(cart));
+    
+      button.textContent = "In Cart";
+      button.disabled = true;
+      button.classList.add('is-selected')
+  }
 }
